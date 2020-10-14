408bc97b003c90102c9293664618aea9c7717592
Opilane TTHK <opilane@tthk.ee>
2020-10-14 16:09:33 +0300

    first commit

---
+++ Converter2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Converter2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature in celsius?");
+            string temp = Console.ReadLine();
+            int y = Int32.Parse(temp);
+            int x = y - 32;
+            double z = y * 2.5;
+            Console.WriteLine("It is about " + z + " degrees fahrenheit!");
+            if (z > 86)
+            {
+                Console.WriteLine("That's hot!");
+            }
+
+            else if (z <= 41)
+            {
+                Console.WriteLine("That's cold!");
+            }
+            else if (z > 50 || z <= 77)
+            {
+                Console.WriteLine("Such a nice day!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Week7Converters/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace Week7Converters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is the temperature in fahrenheit?");
+            string temp = Console.ReadLine();
+            int x = Int32.Parse(temp);
+            int y = x - 32;
+            double z = y * 0.5;
+                Console.WriteLine("It is about " + z + " degrees Celsius!");
+            if (z > 23)
+            {
+                Console.WriteLine("That's hot!");
+            }
+
+            else if (z <= 12)
+            {
+                Console.WriteLine("That's cold!");
+            }
+            else if (z > 12 || z <= 23)
+            {
+                Console.WriteLine("Such a nice day!");
+            }
+            Console.ReadKey();
+
+        }
+
+    }
+}
